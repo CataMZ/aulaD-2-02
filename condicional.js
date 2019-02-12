2fa9b2eeef4ff30d369f0ea354eb61381c351cf0
Catarina <catarina.manzieri@gmail.com>
2019-02-12 12:13:25 -0200

    Entender sobre o basico de js

---
+++ condicional.js
@@ -0,0 +1,19 @@
+console.log("Hello world!");
+
+let nota = Number(prompt("Qual a nota do aluno?"));
+
+console.log(nota);
+
+// alert("A nota do aluno é: " + nota);
+// alert(`A nota do aluno é ${nota}.`);
+
+// if (nota >=6) {
+//     console.log('Parabéns, o aluno foi aprovado!');
+// }
+// else if (nota >= 5.5 && nota < 6){
+//     alert('Parabéns, você foi aprovado, agradeça o sistema.');
+// }
+// else {
+//     console.log('Parece que sua nota foi insuficiente, converse com a coordenação');
+// }
+// Number
---
+++ script.js
@@ -0,0 +1,93 @@
+
+/*
+contador
+while (CONDIÇÃO) {
+    execução
+    
+    passo
+} */
+
+// let contador = 0;
+
+// while(contador < 10) {
+//     console.log(contador);
+
+//     // contador = contador + 1;
+//     // contador += 1;
+//     contador ++;
+// }
+
+// let nota = Number(prompt('Qual a nota do aluno?'));
+// let i = 0;
+// // contador é escrito como i
+// // i, j, k...
+
+// while (isNaN(nota) && contador < 5) {
+//     nota = Number(prompt('Por favor, digite um número. Qual a nota do aluno?'));
+
+//     i ++;
+// }
+
+// if (i >= 5) {
+//     alert('Você chegou no limite de tentativas, por favor tente novamente mais tarde.')
+// }
+
+// alert(`A nota do aluno é ${nota}`);
+
+// let valores = [15, 2, -10, 227, 458, 300];
+
+// let i = 0;
+// while(i < vetor.length) {
+//      console.log(vetor[i]);
+
+//      i++;
+//  }
+
+// for (let valor of valores) {
+     // VERIFICAR SE O VALOR É PAR OU ÍMPAR E AVISAR NO CONSOLE.LOG O RESULTADO
+    
+//     if(valor % 2 === 0) {
+//         console.log(`O número ${valor} é par.`);
+//     }    
+//     else{
+//         console.log(`O número ${valor} é ímpar.`);
+//     }
+    
+// }
+
+let joaozinho = {
+    nome: "João",
+    sobrenome: "da Silva",
+    idade: 17,
+    namorada: undefined,
+    cachorros: [
+        {
+            nome: "Rex",
+            raca: "Tomba-potes"
+        },
+        {
+            nome: "Mila",
+            raca: "Tomba-lata"
+        }
+    ]
+};
+
+let mariazinha = {
+    nome: "Maria",
+    sobrenome: "Consuelo",
+    idade: 22,
+    namorado: undefined
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
